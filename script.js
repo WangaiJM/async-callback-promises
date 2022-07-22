fa6d43c898d2385cde640b215da0056a06c7b7c2
WangaiJM <jahkeyjohn@gmail.com>
2022-07-22 22:43:51 +0300

    ajax into

--- script.js
+++ script.js
@@ -1,57 +1,87 @@
-const btn = document.querySelector(".btn");
+// const btn = document.querySelector(".btn");
 
-btn.addEventListener("click", toggle); // toggle used as  a callback
+// btn.addEventListener("click", toggle); // toggle used as  a callback
 
-// const timer = setInterval(toggle, 50);
+// // const timer = setInterval(toggle, 50);
 
-function toggle() {
-  btn.classList.toggle("change");
+// function toggle() {
+//   btn.classList.toggle("change");
+// }
+
+// // More on callbacks
+
+// function first() {
+//   console.log("First action");
+// }
+// function second() {
+//   console.log("Second action");
+// }
+// function third(number) {
+//   console.log(number);
+// }
+
+// setTimeout(first, 2000); //timeout callback
+
+// // Adding params to callback
+// setTimeout(() => {
+//   third(10);
+// }, 3000);
+
+// // Promises
+
+// const done = false;
+
+// const isItYetDone = new Promise((resolve, reject) => {
+//   if (done) {
+//     const reason = "It is Done";
+//     resolve(reason);
+//   } else {
+//     const why = "we are waiting";
+//     reject(why);
+//   }
+// });
+
+// isItYetDone.then(
+//   (result) => console.log(result),
+//   (error) => console.log(error)
+// );
+
+// let promise = new Promise((resolve, reject) => {
+//   setTimeout(resolve(123), 1000);
+//   reject("Why now?");
+// });
+
+// promise
+//   .then(
+//     (result) => console.log(result),
+//     (error) => console.log(error)
+//   )
+//   .catch(console.log("An error occured"));
+
+// const response = fetch("./data.json");
+
+// const info = new Promise((resolve, reject) => {
+//   resolve(console.log(Response));
+//   reject("An error occured");
+// });
+
+// info.then(
+//   (result) => console.log(result),
+//   (error) => console.log(error)
+// // );
+function ajax() {
+  let xhr = new XMLHttpRequest();
+
+  xhr.open("GET", "./data.json", true);
+
+  xhr.onload = function () {
+    if ((xhr.status = 200)) {
+      const data = JSON.parse(xhr.response);
+      console.log(data);
+    }
+  };
+
+  xhr.send();
 }
 
-// More on callbacks
-
-function first() {
-  console.log("First action");
-}
-function second() {
-  console.log("Second action");
-}
-function third(number) {
-  console.log(number);
-}
-
-setTimeout(first, 2000); //timeout callback
-
-// Adding params to callback
-setTimeout(() => {
-  third(10);
-}, 3000);
-
-// Promises
-
-const done = false;
-
-const isItYetDone = new Promise((resolve, reject) => {
-  if (done) {
-    const reason = "It is Done";
-    resolve(reason);
-  } else {
-    const why = "we are waiting";
-    reject(why);
-  }
-});
-
-isItYetDone.then(
-  (result) => console.log(result),
-  (error) => console.log(error)
-);
-
-let promise = new Promise((resolve, reject) => {
-  setTimeout(resolve(123), 1000);
-  reject("Why now?");
-});
-
-promise.then(
-  (result) => console.log(result),
-  (error) => console.log(error)
-);
+ajax();
